2195003e428ccf2b63b402e11bcc740b473a9076
jacquline <jacqulinewangu@gmail.com>
2019-07-01 09:53:02 +0300

    added importations in the views.py file

--- AWARDS_APP/views.py
+++ AWARDS_APP/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from .models import Project,Profile
+from .forms import ProjectForm,ProfileForm,VoteForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
