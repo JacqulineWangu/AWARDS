0702a3ecdec9c837e26b6f99570b5e6173f859aa
jacquline <jacqulinewangu@gmail.com>
2019-07-01 10:07:47 +0300

    added on the serializer importing response APIView

--- AWARDS_APP/views.py
+++ AWARDS_APP/views.py
@@ -4,5 +4,9 @@ from django.contrib.auth.models import User
 from .models import Project,Profile
 from .forms import ProjectForm,ProfileForm,VoteForm
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializer import ProfileSerializer,ProjectSerializer
 
 # Create your views here.
